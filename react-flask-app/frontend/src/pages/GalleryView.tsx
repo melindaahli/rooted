a34af2f545f842455663cae7f62437918f7fab14
Sherry Tram <smtram@uci.edu>
2026-02-28 15:27:42 -0800

    Add gallery scroll changes and assets

--- react-flask-app/frontend/src/pages/GalleryView.tsx
+++ react-flask-app/frontend/src/pages/GalleryView.tsx
@@ -1,18 +1,70 @@
 import shelf from "../assets/shelf.svg";
+import arrow from "../assets/nocircle_arrow.svg";
+import plant from "../assets/plant_placeholder.png";
 
 function GalleryView() {
     return (
      
     <div className="blue_background">
-        <nav className="nav"><a href="DashboardView.tsx">Dashboard</a></nav>
+        <div className="nav">
+            <img src={arrow} style={{ width: '8px', height: 'auto', transform: 'rotate(180deg)' }}/>
+            <p className="nav_text"><a href="DashboardView.tsx">Dashboard</a></p>
+        </div>
+        
         <div className="tan-card">
-            <div>
+            <div className="header">
                 <h1 className="gallery-title">My Garden</h1>
-                <button>+ Add Plant</button>
+                <button className="add_plant">+ Add Plant</button>
             </div>
-            <div className="parent-shelf">
-                <img src={shelf} style={{ width: '90%', height: 'auto' }}/>
+            
+            <div className="plant_scroll">
+                <div className="plant-section">
+                    <div className="plants-row">
+                        <div>
+                            <p className="plant_text"><a href="DashboardView.tsx">plant name</a></p>
+                            <img src={plant} alt="plant" />
+                        </div>
+                        <div>
+                            <p className="plant_text"><a href="DashboardView.tsx">plant name</a></p>
+                            <img src={plant} alt="plant" />
+                        </div>
+                    </div>
+
+                    <img src={shelf} alt="shelf"className="shelf-img"/>
+                </div>
+
+                <div className="plant-section">
+                    <div className="plants-row">
+                        <div>
+                            <p className="plant_text"><a href="DashboardView.tsx">plant name</a></p>
+                            <img src={plant} alt="plant" />
+                        </div>
+                        <div>
+                            <p className="plant_text"><a href="DashboardView.tsx">plant name</a></p>
+                            <img src={plant} alt="plant" />
+                        </div>
+                    </div>
+
+                    <img src={shelf} alt="shelf"className="shelf-img"/>
+                </div>
+
+                <div className="plant-section">
+                    <div className="plants-row">
+                        <div>
+                            <p className="plant_text"><a href="DashboardView.tsx">plant name</a></p>
+                            <img src={plant} alt="plant" />
+                        </div>
+                        <div>
+                            <p className="plant_text"><a href="DashboardView.tsx">plant name</a></p>
+                            <img src={plant} alt="plant" />
+                        </div>
+                    </div>
+
+                    <img src={shelf} alt="shelf"className="shelf-img"/>
+                </div>
+                
             </div>
+            
         </div>
     </div>
     
